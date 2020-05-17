8e685ae6276d29c463543c4b5ed6ff1819aa626e
uedakousuke <63963671+uedakousuke@users.noreply.github.com>
2020-05-17 15:25:48 +0900

    Add files via upload

---
+++ controllers/errorController.js
@@ -0,0 +1,15 @@
+const httpStatus =require("http-status-codes");
+exports.pageNOTFOUNDError =(req,res) => {
+let errorCode =
+httpStatus.NOT_FOUND;
+res.status(errorCode);
+res.render("error")
+};
+
+exports.internalServerError = (error,req,res,next) => {
+    let errorCode =
+    httpStatus.INTERNAL_SERVER_ERROR;
+    console.log(`ERROR occurred:${error.stack}`)
+    res.status(errorCode);
+    res.send(`${errorCode} | Sorry,our application is taking a nap!`);
+};
---
+++ controllers/homeController.js
@@ -0,0 +1,32 @@
+//個々の経路のためにコールバック関数を追加
+exports.showCourses= (req,res) => {
+    res.render("courses");
+}; 
+exports.showSignUp = (req,res) => {
+    res.render("contact");
+};
+exports.postedSignUpForm = (req,res) => {
+    res.render("thanks");
+};
+//コースの配列を定義
+var courses = [
+    {
+        title:"Event Driven Cakes",
+        cost:50
+    },
+    {
+        title:"Asynchronous Artichoke",
+        cost:25
+    },
+    {
+        title:"Object Oriented Orange Juice",
+        cost: 10
+    }
+];
+
+exports.showCourses = (req,res) => {
+    res.render("courses", {
+        //コースの配列をビューに渡す
+        offeredCourses: courses
+    });
+};
